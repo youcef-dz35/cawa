437630997bc3d1ad666fefced33df7cfc2a17e49
KEBIR Youcef <kebiryoucef07@gmail.com>
2020-11-25 15:15:39 +0100

    intializing the project for the first time controllers are set up along with hte models

---
+++ database/migrations/2020_11_24_185215_create_authors_table.php
@@ -0,0 +1,35 @@
+php artisan jetstream:install livewire<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->integer("num")->unique();
+            $table->string("first_name");
+            $table->string("last_name");
+            $table->date("dob");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
---
+++ database/migrations/2020_11_24_185249_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('issn');
+            $table->unsignedBigInteger('author_id');
+            $table->string("title");
+            $table->string("resume");
+            $table->integer("nbpage");
+            $table->timestamps();
+            $table->foreign('author_id')
+                ->references('id')
+                ->on('authors')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
